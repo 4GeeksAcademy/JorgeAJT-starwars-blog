194b1b0d6c6122ff061389aeeda2d5a08637ed43
Jorge Jiménez <154094092+JorgeAJT@users.noreply.github.com>
2024-04-21 00:32:35 +0000

    added photo's films

--- src/js/views/films.js
+++ src/js/views/films.js
@@ -13,11 +13,12 @@ export const Films = () => {
 	return (
 	<div className="text-center mt-5">
 		<h1>Films</h1>
-		<div className="row d-flex">
+		<div className="row d-flex mt-4">
 		{store.films.map((film, index) => 
 		<div className="col-12 col-md-6 col-xl-3 my-xl-2">
 			<Card 
 			key={index+1}
+            image={`https://raw.githubusercontent.com/tbone849/star-wars-guide/master/build/assets/img/films/${index+1}.jpg`}
 			title={film.title}
 			/>
 		</div>
